475a939f8d5e17f3cfa1045e2caec091a31a8b71
npbehunin <npbehunin@gmail.com>
2020-02-05 18:00:45 -0700

    Added takoto v3 with controls and adjusted control script

--- Simple Control Creation Script.py
+++ Simple Control Creation Script.py
@@ -6,14 +6,14 @@ def control(sels = []): #'circle' is the default value.
     if not sels:
         sels=cmds.ls(sl=True)
     
-    n_sels = []
+    n_ctrls = []
     
     for sel in sels:
         pos = cmds.xform(sel, q=True, ws=True, translation=True)
         orient = cmds.xform(sel, q=True, ws=True, rotation=True)
         size = cmds.xform(sel, q=True, ws=True, scale=True)
         
-        ctrl = cmds.circle(c=[0,0,0], nr=[0,1,0], sw=360, r=1, d=3, ut=0, tol=0.01, s=8 ch=1)[0]
+        ctrl = cmds.circle(c=[0,0,0], nr=[0,1,0], sw=360, r=2, d=3, ut=0, tol=0.01, s=8, ch=1)[0]
         
         cmds.xform(ctrl, ws=True, translation=pos)
         cmds.xform(ctrl, ws=True, rotation=orient)
@@ -30,6 +30,8 @@ def control(sels = []): #'circle' is the default value.
 def group(sels=[]):
     if not sels:
         sels=cmds.ls(sl=True)
+        
+    n_sels = []
     
     for sel in sels:
         pos = cmds.xform(sel, q=True, ws=True, translation=True)
